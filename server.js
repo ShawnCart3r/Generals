e62d012dcfa16123b8a041ab2ab174f78823d862
asmilingmuffin <53487411+asmilingmuffin@users.noreply.github.com>
2021-05-23 19:33:39 -0400

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('generals.json');
-const middlewares = json.defaults();
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
